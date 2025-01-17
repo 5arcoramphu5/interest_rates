1316e53e97667ad5db94d7746bf97f6dac00f2bc
Vittoria Condorelli <vittoriacondorelli005@gmail.com>
2025-01-17 19:18:23 +0100

    calibrating Vasicek

--- SDE_schemes.py
+++ SDE_schemes.py
@@ -1,6 +1,7 @@
 import numpy as np
 import sympy as sym
 from sympy.abc import x, t
+from abc import ABC, abstractmethod
 from typing import Tuple
 from collections.abc import Callable
 
@@ -16,73 +17,94 @@ class SDE:
         self.b_der = sym.lambdify([x, t], b_der_sym)
         
 
+class SDESolver(ABC):
+    """
+    Abstract base class for solving Stochastic Differential Equations (SDEs).
+
+    Methods
+    -------
+    performSimulation(sde: SDE, r0: float, t0: float, tN: float, N: int) -> Tuple[np.array, np.array]
+        Method to perform the simulation of the SDE.
+    """
+    
+    @staticmethod
+    @abstractmethod
+    def performSimulation(sde: SDE, r0: float, t0: float, tN: float, N: int)-> Tuple[np.array, np.array]:
+        pass
+
+
 def dW(dt: float):
     return np.random.normal(0, np.sqrt(dt))
     
-def euler_maruyama(sde: SDE, y0: float, t0: float, tN: float, N: int) -> Tuple[np.array, np.array]:
-    """
-    Euler-Maruyama scheme for solving stochastic differential equations of form dX_t = a(X_t, t)dt + b(X_t, t)dW_t.
+class EulerMaruyamaSolver(SDESolver):
 
-    Parameters:
-    sde: SDE
-        Object representing the SDE.
-    x0: float
-        The initial condition X_0 = x0.
-    t0: float
-        The initial time.
-    tN: float
-        The final time.
-    N: int
-        The number of time steps.
+    @staticmethod
+    def performSimulation(sde: SDE, r0: float, t0: float, tN: float, N: int) -> Tuple[np.array, np.array]:
+        """
+        Euler-Maruyama scheme for solving stochastic differential equations of form dX_t = a(X_t, t)dt + b(X_t, t)dW_t.
 
-    Returns:
-    t: numpy array
-        The time grid.
-    x: numpy array
-        The solution of the SDE at the time grid points.
-    """
-    dt = (tN - t0) / N
-    t = np.linspace(t0, tN, N+1)
+        Parameters:
+        sde: SDE
+            Object representing the SDE.
+        x0: float
+            The initial condition X_0 = x0.
+        t0: float
+            The initial time.
+        tN: float
+            The final time.
+        N: int
+            The number of time steps.
 
-    Y = np.zeros(N+1)
-    Y[0] = y0
+        Returns:
+        t: numpy array
+            The time grid.
+        x: numpy array
+            The solution of the SDE at the time grid points.
+        """
+        dt = (tN - t0) / N
+        t = np.linspace(t0, tN, N+1)
 
-    for i in range(N):
-        Y[i+1] = Y[i] + sde.a(Y[i], t[i]) * dt + sde.b(Y[i], t[i]) * dW(dt)
+        Y = np.zeros(N+1)
+        Y[0] = r0
 
-    return t, Y
+        for i in range(N):
+            Y[i+1] = Y[i] + sde.a(Y[i], t[i]) * dt + sde.b(Y[i], t[i]) * dW(dt)
 
+        return t, Y
 
-def milstein(sde: SDE, y0: float, t0: float, tN: float, N: int) -> Tuple[np.array, np.array]:
-    """
-    Milstein scheme for solving stochastic differential equations of form dX_t = a(X_t)dt + b(X_t)dW_t.
+class MilsteinSolver(SDESolver):
 
-    Parameters:
-    sde: SDE
-        Object representing the SDE.
-    x0: float
-        The initial condition X_0 = x0.
-    t0: float
-        The initial time.
-    tN: float
-        The final time.
-    N: int
-        The number of time steps.
+    @staticmethod
+    def performSimulation(sde: SDE, r0: float, t0: float, tN: float, N: int) -> Tuple[np.array, np.array]:
+        """
+        Milstein scheme for solving stochastic differential equations of form dX_t = a(X_t)dt + b(X_t)dW_t.
 
-    Returns:
-    t: numpy array
-        The time grid.
-    x: numpy array
-        The solution of the SDE at the time grid points.
-    """
-    dt = (tN - t0) / N
-    t = np.linspace(t0, tN, N+1)
+        Parameters:
+        sde: SDE
+            Object representing the SDE.
+        x0: float
+            The initial condition X_0 = x0.
+        t0: float
+            The initial time.
+        tN: float
+            The final time.
+        N: int
+            The number of time steps.
 
-    Y = np.zeros(N+1)
-    Y[0] = y0
+        Returns:
+        t: numpy array
+            The time grid.
+        x: numpy array
+            The solution of the SDE at the time grid points.
+        """
+        dt = (tN - t0) / N
+        t = np.linspace(t0, tN, N+1)
 
-    for i in range(N):
-        currdW = dW(dt)
-        Y[i+1] = Y[i] + sde.a(Y[i], t[i]) * dt + sde.b(Y[i], t[i]) * currdW + sde.b(Y[i], t[i]) * sde.b_der(Y[i], t[i]) / 2 * (currdW**2 - dt)
+        Y = np.zeros(N+1)
+        Y[0] = r0
 
-    return t, Y
+        for i in range(N):
+            currdW = dW(dt)
+            Y[i+1] = Y[i] + sde.a(Y[i], t[i]) * dt + sde.b(Y[i], t[i]) * currdW + sde.b(Y[i], t[i]) * sde.b_der(Y[i], t[i]) / 2 * (currdW**2 - dt)
+
+        return t, Y
--- models.py
+++ models.py
@@ -2,8 +2,9 @@ import numpy as np
 import sympy as sym
 from abc import ABC, abstractmethod
 from SDE_schemes import SDE
+from pandas import Series
 
-class Model(ABC):
+class InterestRateModel(ABC):
     """
     Abstract class for defining a interest rate model.
     """
@@ -15,9 +16,36 @@ class Model(ABC):
         """
         pass
 
-class VasicekModel(Model):
+    @abstractmethod
+    def calibrate(self, rates: Series, dt: float) -> float:
+        """
+        Calibrates the model to market data. Returns r0.
+        """
+        pass
+
+
+class VasicekModel(InterestRateModel):
+    """
+    Vasicek Model for interest rate modeling.
+    The Vasicek model is a type of one-factor short rate model that describes the evolution of interest rates. 
+
+    where:
+    - alpha: speed of reversion to the mean
+    - theta: long-term mean level of the interest rate
+    - sigma: volatility of the interest rate
+    - r0: initial interest rate
+    Methods:
+        getSDE() -> SDE:
+            Returns the stochastic differential equation (SDE) for the Vasicek model.
+
+        calibrate(rates, dt)
+            Calibrates the model parameters based on historical interest rate data.
+            Args:
+                rates (Series): Historical interest rate data.
+                dt (float): Time increment.
+    """
     
-    def __init__(self, theta: float, alpha: float, sigma: float):
+    def __init__(self, alpha: float = 0, theta: float = 0, sigma: float = 0):
         self.alpha = alpha
         self.theta = theta
         self.sigma = sigma
@@ -31,7 +59,24 @@ class VasicekModel(Model):
         
         return SDE(a, b)
     
-class CIRModel(Model):
+    def calibrate(self, rates: Series, dt:float) -> None:
+        N = len(rates)
+        
+        Sx = sum(rates.iloc[0:(N-1)])
+        Sy = sum(rates.iloc[1:N])
+        Sxx = np.dot(rates.iloc[0:(N-1)], rates.iloc[0:(N-1)])
+        Sxy = np.dot(rates.iloc[0:(N-1)], rates.iloc[1:N])
+        Syy = np.dot(rates.iloc[1:N], rates.iloc[1:N])
+
+        theta = (Sy * Sxx - Sx * Sxy) / (N * (Sxx - Sxy) - (Sx**2 - Sx*Sy))
+        kappa = -np.log((Sxy - theta * Sx - theta * Sy + N * theta**2) / (Sxx - 2*theta*Sx + N*theta**2)) / dt
+        a = np.exp(-kappa * dt)
+        sigmah2 = (Syy - 2*a*Sxy + a**2 * Sxx - 2*theta*(1-a)*(Sy - a*Sx) + N*theta**2 * (1-a)**2) / N
+        self.sigma = np.sqrt(sigmah2*2*kappa / (1-a**2))
+        self.theta = theta * kappa
+        self.alpha = kappa
+
+class CIRModel(InterestRateModel):
     
     def __init__(self, theta: float, alpha: float, sigma: float):
         self.alpha = alpha
@@ -47,18 +92,5 @@ class CIRModel(Model):
         
         return SDE(a, b)
     
-
-class RBModel(Model):
-    
-    def __init__(self, theta: float, sigma: float):
-        self.theta = theta
-        self.sigma = sigma
-
-    def getSDE(self) -> SDE:
-        def a(x: float, t: float) -> float:
-            return self.theta * x
-        
-        def b(x: sym.Float, t: sym.Float) -> sym.Float:
-            return self.sigma * x
-        
-        return SDE(a, b)
+    def calibrate(self, rates: Series, dt:float) -> None:
+        pass
